e36c88a233c1b11a3c432c0c04c704c4da4b508c
Nelson Anderiansyah <nelsenandreansyah@gmail.com>
2023-04-12 17:34:07 +0700

    2 & 3 & 4

---
+++ app/src/main/java/com/si61/sqlitemahasiswa/AdapterMahasiswa.java
@@ -0,0 +1,54 @@
+package com.si61.sqlitemahasiswa;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterMahasiswa extends RecyclerView.Adapter<AdapterMahasiswa.ViewHolderMahasiswa> {
+    private Context ctx;
+    private ArrayList arrId, arrNpm, arrNama, arrProdi;
+
+    public AdapterMahasiswa(Context ctx, ArrayList arrId, ArrayList arrNpm, ArrayList arrNama, ArrayList arrProdi) {
+        this.ctx = ctx;
+        this.arrId = arrId;
+        this.arrNpm = arrNpm;
+        this.arrNama = arrNama;
+        this.arrProdi = arrProdi;
+    }
+
+    @NonNull
+    @Override
+    public AdapterMahasiswa.ViewHolderMahasiswa onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(ctx).inflate(R.layout.list_item_mahasiswa, parent, false);
+        return new ViewHolderMahasiswa(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterMahasiswa.ViewHolderMahasiswa holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolderMahasiswa extends RecyclerView.ViewHolder{
+        TextView tvId, tvNpm, tvNama, tvProdi;
+
+        public ViewHolderMahasiswa(@NonNull View itemView) {
+            super(itemView);
+            tvId = itemView.findViewById(R.id.tv_id);
+            tvNpm = itemView.findViewById(R.id.tv_npm);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvProdi = itemView.findViewById(R.id.tv_prodi);
+        }
+    }
+}
